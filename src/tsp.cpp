dc6037078bf03cbe2fdfcb4e56062143703a0ba3
Gustavo Guedes <gustavoipguedes@gmail.com>
2021-11-30 23:05:35 -0300

    fix: Mudança de lista para heap

--- src/tsp.cpp
+++ src/tsp.cpp
@@ -13,13 +13,10 @@ using namespace std;
 
 struct Vertice{
     int x,y;
-    int visit;
-    double c;
-    int p;
     int adj_size;
     vector<int> adj;
 
-    Vertice(int x, int y):x(x),y(y), visit(0), c(INF),adj_size(0){}
+    Vertice(int x, int y):x(x),y(y),adj_size(0){}
     string to_string(){ return std::to_string(x) +" "+ std::to_string(y); }
     void add_adj(int i){
         adj.push_back(i);
@@ -29,18 +26,21 @@ struct Vertice{
 
 struct Graph {
     int n;
-    Vertice* v;
+    vector<Vertice> v;
 
-    Graph(int n):n(n),v(NULL){
-        v = (Vertice*)malloc(n*sizeof(Vertice));
+    Graph():n(0){}
+    void add_vertice(Vertice b){ v.push_back(Vertice(b.x, b.y)); n+=1;}
+    void add_vertice(int x, int y){
+        v.push_back(Vertice(x,y));
+        n+=1;
     }
-    Graph(){}
     double distance(Vertice a, Vertice b){ return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));}
     void add_edge(int a, int b){
-        double dist = distance(v[a], v[b]);
         v[a].add_adj(b);
-        v[b].add_adj(a);
     }
+
+    //*******************************************
+    //TODO remover depois
     string print_adj(int a){
         string tostring = "[";
         for (int i=0; i<v[a].adj_size; i++)
@@ -55,10 +55,58 @@ struct Graph {
         }
         return tostring;
     }
+    //********************************************
+};
+struct Heap{
+    vector<int> v;
+    vector<int> cost;
+    int n;
+    Heap():n(0){v.push_back(0);};
+    
+    void add_vertice(int i, int c){
+        v.push_back(i);
+        cost.push_back(c);
+        n++;
+    }
+    int extract_min(){
+        int min_heap = v[1];
+        v[1] = v[n];
+        n--;
+        min_heapify(1);
+        return min_heap;
+    }
+    void update(int i){
+        int p = i/2;
+        if(p>0 && cost[v[i]]<cost[v[p]]){
+            change(i,p);
+            update(p);
+        }
+    }
+    void min_heapify(int i){
+        int e = i*2;
+        int d = i*2+1;
+        int m = i;
+        if(e<=n && cost[v[e]]<cost[v[m]]) m=e;
+        if(d<=n && cost[v[d]]<cost[v[m]]) m=d;
+
+        if(m!=i){
+            change(m,i);
+            min_heapify(m);
+        }
+    }
+    void change(int i, int j){
+        int aux = v[i];
+        v[i]=v[j];
+        v[j]=aux;
+    }
+    void print(){
+        for( int i=1; i<=n; i++)
+            cout << v[i] << ":" << cost[v[i]] << " ";
+        cout << endl;
+    }
 };
 
-
-Graph T, H;
+Graph T;
 
 // ---------------- LEITURA DO ARQUIVO --------------------
 Graph read_points(char *file_name){
@@ -67,34 +115,39 @@ Graph read_points(char *file_name){
     if (fp == NULL)
 	{
 		fprintf(stderr, "Falha ao ler a entrada.\n");
-        return Graph(0);
+        return Graph();
 	}
     int n;
     fscanf(fp,"%d", &n);
-    Graph R(n);
+    Graph R;
 
-    for(int i=0; i<R.n; i++){
+    for(int i=0; i<n; i++){
         int x, y;
         fscanf(fp,"%d %d", &x, &y);
-        R.v[i]=Vertice(x,y);
+        R.add_vertice(x,y);
     }
 
     fclose(fp);
-
+    
     return R;
 }
 
-void save_graph(Graph G, string name){
+void save_tree(string name){
     std::ofstream example(name);
     std::ostream& os = example;
 
-    for(int i=0; i<G.n; i++){
-        for(int j=0; j<G.v[i].adj_size; j++){
-            os << G.v[i].to_string() << endl;
-            os << G.v[G.v[i].adj[j]].to_string() << endl;
+    for(int i=0; i<T.n; i++){
+        for(int j=0; j<T.v[i].adj_size; j++){
+            int v=T.v[i].adj[j];
+            if(i!=v){
+                os << T.v[i].to_string() << endl;
+                os << T.v[v].to_string() << endl;
+            }
         }
     }
 }
+
+/*
 void save_cycle(Graph G, string name){
     std::ofstream example(name);
     std::ostream& os = example;
@@ -107,56 +160,44 @@ void save_cycle(Graph G, string name){
 void print_graph(Graph G){
     cout << G.to_string() << endl;
 }
+*/
 // ------------------------------------------------------
-clock_t total = 0;
-int extract_min(Graph G){
-    clock_t t;
-    t = clock();
 
-    int u=-1;
-    for(int i=0; i<G.n; i++){
-        if(u==-1 && G.v[i].visit==0) u=i;
-        if(G.v[i].visit==0 && G.v[i].c<G.v[u].c)
-            u=i;
-    }
-    t = clock()-t;
-    total+=t;
-    return u;
-}
 Graph mst_prim(Graph G, int r){
-    for(int i=0; i<G.n; i++){
-        G.v[i].c=INF;
-        G.v[i].visit=0;
-    }
-    G.v[r].c=0;
-    for(int i=0; i<G.n; i++){
-        int u = extract_min(G);
-        G.v[u].visit=1;
-        
-        double c;
-        double dist;
-        for(int v=0 ; v<G.n; v++){
-            dist = G.distance(G.v[u],G.v[v]);
-            if(dist < G.v[v].c && !G.v[v].visit){
-                G.v[v].c = dist; 
-                G.v[v].p = u;
-            }
-        }
-    }
+    Heap H;
+    vector<int> visited(G.n);
+    vector<int> parent(G.n);
 
     
-    clock_t t;
-    t = clock();
-    Graph P(G.n);
-    for(int i=0; i<G.n; i++){P.v[i]=Vertice(G.v[i].x,G.v[i].y);}
     for(int i=0; i<G.n; i++){
-        //cout << i << " pai:" << G.v[i].p << endl;
-        if(i!=r) P.add_edge(i,G.v[i].p);
+        H.add_vertice(i,INF);
+        T.add_vertice(G.v[i]);
+        visited[i]=0;
     }
-    t = clock()-t;
-    cout << (float)t/CLOCKS_PER_SEC << " segundos criando o grafo da arvore minima" << endl;
-    return P;
+    H.cost[r]=0;
+    parent[r]=r;
+    H.update(r+1);
+
+    while(H.n>0){
+        int u = H.extract_min();
+        T.add_edge(parent[u], u);
+        visited[u]=1;
+        
+        for(int i=1; i<=H.n; i++){
+            int v = H.v[i];
+            float dist = G.distance(G.v[u],G.v[v]);
+            if(dist < H.cost[v]){
+                H.cost[v] = dist;
+                parent[v] = u; 
+                H.update(i);
+            }
+        }
+        
+    }
+
+    return T;
 }
+/*
 int dfs(Graph T, int u, int ordem[], int pos){
     ordem[pos++]=u;
     T.v[u].visit=1;
@@ -182,19 +223,18 @@ Graph pre_ordem(Graph T, int r){
     for(int i=0; i<T.n; i++){H.v[i]=Vertice(T.v[ordem[i]].x,T.v[ordem[i]].y);}
     return H;
 }
+*/
 void approx_tsp_tour(Graph G){
     int r = 0;
     //for(int i=0; i<G.n; i++) if(G.v[i].y<G.v[r].y) r=i;
-    cout << "r: " << r << endl;
 
     cout << "Calculando Prim..." << endl;
     T = mst_prim(G, r);
     cout << "Ok!" << endl;
     
-    cout << (float)total/CLOCKS_PER_SEC << " segundos extraindo o minimo" << endl;
-
-    cout << "Calculando Pré Ordem..." << endl;
-    H = pre_ordem(T, r);
+  
+    //cout << "Calculando Pré Ordem..." << endl;
+    //H = pre_ordem(T, r);
 }
 
 int main(int argc, char *argv[]){
@@ -218,9 +258,9 @@ int main(int argc, char *argv[]){
     
     cout << "Salvando..." << endl;
     cout << "Salvando a árvore mínima..." << endl;
-    save_graph(T, "build/tree.txt");
-    cout << "Salvando o ciclo..." << endl;
-    save_cycle(H, "build/cycle.txt");
+    save_tree("build/tree.txt");
+    //cout << "Salvando o ciclo..." << endl;
+    //save_cycle(H, "build/cycle.txt");
     cout << "Ok!" << endl;
     
     return 0;
